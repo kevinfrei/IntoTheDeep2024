eb58ef8c4d08acd4a5edd92dffc9dab844d1c50a
Kevin Frei <kevinfrei@hotmail.com>
2024-11-01 23:30:11 -0700

    Run to Position try out

---
+++ LearnBot/src/main/java/org/firstinspires/ftc/learnbot/subsystems/RunToPositionSubsystem.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.learnbot.subsystems;
+
+import com.technototes.library.logger.Loggable;
+import com.technototes.library.subsystem.Subsystem;
+
+public class RunToPositionSubsystem implements Subsystem, Loggable {
+    
+}
